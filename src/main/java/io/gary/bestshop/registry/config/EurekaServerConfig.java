f07bc2054d2873e300ff9262e3e57887500832bd
Gary Zhang <gary.zhang@technest.es>
2018-03-29 00:25:58 +0200

    first commit

---
+++ src/main/java/io/gary/bestshop/registry/config/EurekaServerConfig.java
@@ -0,0 +1,10 @@
+package io.gary.bestshop.registry.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfig {
+
+}
